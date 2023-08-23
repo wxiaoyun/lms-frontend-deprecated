941dbe78a24e43f467bdbd0c9e55cbe748328169
ForAeons <w.xy020203@gmail.com>
2023-08-23 20:40:33 +0800

    add env file and captain githook

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const DEFAULR_PORT = "3000";
+const PORT = Number(process.env.PORT || DEFAULR_PORT);
+
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		port: PORT,
+	},
 	plugins: [react()],
 });
