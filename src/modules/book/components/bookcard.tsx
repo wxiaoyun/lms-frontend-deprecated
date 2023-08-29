bda44f3472ac0f18065abd7830689a4fa020bf15
ForAeons <w.xy020203@gmail.com>
2023-08-29 19:06:52 +0800

    typo

--- src/modules/book/components/bookcard.tsx
+++ src/modules/book/components/bookcard.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 export const BookCard: React.FC = () => {
-  return (
-    <div>BookCard: React.FC</div>
-  )
-}
+  return <div>BookCard: React.FC</div>;
+};
 
-export default BookCard: React.FC
+export default BookCard;
